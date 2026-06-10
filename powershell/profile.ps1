a692a9d8ea8abe94c57d5559c4253de211e1e872
xxx <xxx@xyz.com>
2026-06-11 01:19:41 +0200

    init

---
+++ powershell/profile.ps1
@@ -0,0 +1,6 @@
+Import-Module -Name Terminal-Icons
+oh-my-posh init pwsh --config 'https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/catppuccin_mocha.omp.json' | Invoke-Expression
+
+function cdotfiles { cd "$env:USERPROFILE\.dotfiles" }
+function vimconf { nvim "$env:USERPROFILE\.dotfiles\nvim\init.lua" }
+function reload { . $PROFILE }
